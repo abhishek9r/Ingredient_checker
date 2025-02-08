38871e511ee4c3cc4f1089931c306dc194213663
Ansh3102 <anshulsharma2002.as@gmail.com>
2025-02-09 01:29:44 +0530

    Authentication done

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import logo from "./images/logo.jpg"
+import Login from "./Login";
+import { useAuth0 } from "@auth0/auth0-react";
 const Header = ()=>{
+    const {user} = useAuth0(); 
+    console.log("Current User" , user); 
     return (
         <div>
             <div className="flex justify-between bg-gray-300 p-1 border-b-1 w-390px h-10 ">
            <img className ="w-8 p-1 border-1" src={logo} alt="logo" />
            <div className="flex justify-around">
-           <div className="font-bold mx-3">Login</div>
+           <div className="font-bold mx-3"><Login/></div>
            <div className="font-bold mx-1">Profile</div>
+
            </div>
             
            </div>
