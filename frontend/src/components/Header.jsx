4ee66d4e95fb7a90af31c9de424df109a15cbff0
Ansh3102 <anshulsharma2002.as@gmail.com>
2025-02-09 01:40:07 +0530

    Login Done

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,8 +3,8 @@ import logo from "./images/logo.jpg"
 import Login from "./Login";
 import { useAuth0 } from "@auth0/auth0-react";
 const Header = ()=>{
-    const {user} = useAuth0(); 
-    console.log("Current User" , user); 
+ 
+
     return (
         <div>
             <div className="flex justify-between bg-gray-300 p-1 border-b-1 w-390px h-10 ">
