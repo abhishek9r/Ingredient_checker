01e7171c9650dd04867fc8ff0caa46066a830ab2
Ansh3102 <anshulsharma2002.as@gmail.com>
2025-02-15 23:47:17 +0530

    Login_LogOut_updated

--- frontend/src/components/Body.jsx
+++ frontend/src/components/Body.jsx
@@ -10,6 +10,7 @@ const Body = ()=>{
             <div className="bg-gray-500 text-center w-90 h-50 rounded-2xl my-10 mx-auto p-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate quas reiciendis dignissimos rem sunt eligendi deleniti reprehenderit voluptatibus provident vel? Animi deserunt ex, velit quisquam alias minus nisi quibusdam neque.</div>
             <img className="w-90 mx-auto my-10 " src={scan} alt="scan_Icon" />
             <button className="bg-blue-600 w-30 font-bold rounded-xl h-15 mx-45 my-10">Scan</button>
+            
         </div>
     )
 }
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-
+import LogoutButton from "./Logout";
 
 const Login = ()=>{
     
-        const { loginWithRedirect, user, isAuthenticated, } = useAuth0();
+        const { loginWithRedirect, user, isAuthenticated } = useAuth0();
         console.log("current user", user); 
         return (
             <div>
-                {isAuthenticated ? user.name : <button onClick={() => loginWithRedirect()}>Log In</button>}
+                {isAuthenticated ? user.name  : <button onClick={() => loginWithRedirect()}>Log In</button>}
             </div>
         )
         
